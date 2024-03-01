3680f4c23f816ec215d1e7da530ad599ec2a17f4
Volkov Nikita <n.volkov1@g.nsu.ru>
2024-03-01 22:03:05 +0700

    try to fix

--- src/components/Table/Table.requests.ts
+++ src/components/Table/Table.requests.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Md5 } from "ts-md5";
 import { Request, RequestItem } from "./Table.types";
+import { request } from "http";
 
 const password = "Valantis_";
 let items: string[] = [];
@@ -27,156 +28,172 @@ const deleteDublicates = (array: RequestItem[]) => {
   return result;
 };
 
-export const filterPrice = async ({ filterString, changeItems }: Request) => {
+export const filterPrice = ({ filterString, changeItems }: Request) => {
   let tryNumber = 0;
-  const now = new Date().toISOString().split("T")[0];
-  const nowRightFormat =
-    now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
-  await axios({
-    method: "post",
-    url: "http://api.valantis.store:40000/",
-    headers: {
-      "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
-    },
-    data: {
-      action: "filter",
-      params: { price: Number(filterString) },
-    },
-  })
-    .then(function (response) {
-      items = response.data.result;
-      getFields({ changeItems });
+  async function request() {
+    const now = new Date().toISOString().split("T")[0];
+    const nowRightFormat =
+      now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
+    await axios({
+      method: "post",
+      url: "http://api.valantis.store:40000/",
+      headers: {
+        "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
+      },
+      data: {
+        action: "filter",
+        params: { price: Number(filterString) },
+      },
     })
-    .catch(function (error) {
-      if (tryNumber === 3) {
-        console.log(error);
-      } else {
-        tryNumber++;
-        filterPrice({ filterString, changeItems });
-      }
-    });
+      .then(function (response) {
+        items = response.data.result;
+        getFields({ changeItems });
+      })
+      .catch(function (error) {
+        if (tryNumber === 3) {
+          console.log(error);
+        } else {
+          tryNumber++;
+          request();
+        }
+      });
+  }
+  request();
 };
 
-export const filterName = async ({ filterString, changeItems }: Request) => {
+export const filterName = ({ filterString, changeItems }: Request) => {
   let tryNumber = 0;
-  const now = new Date().toISOString().split("T")[0];
-  const nowRightFormat =
-    now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
-  await axios({
-    method: "post",
-    url: "http://api.valantis.store:40000/",
-    headers: {
-      "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
-    },
-    data: {
-      action: "filter",
-      params: { product: filterString },
-    },
-  })
-    .then(function (response) {
-      items = response.data.result;
-      getFields({ changeItems });
+  async function request() {
+    const now = new Date().toISOString().split("T")[0];
+    const nowRightFormat =
+      now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
+    await axios({
+      method: "post",
+      url: "http://api.valantis.store:40000/",
+      headers: {
+        "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
+      },
+      data: {
+        action: "filter",
+        params: { product: filterString },
+      },
     })
-    .catch(function (error) {
-      if (tryNumber === 3) {
-        console.log(error);
-      } else {
-        tryNumber++;
-        filterName({ filterString, changeItems });
-      }
-    });
+      .then(function (response) {
+        items = response.data.result;
+        getFields({ changeItems });
+      })
+      .catch(function (error) {
+        if (tryNumber === 3) {
+          console.log(error);
+        } else {
+          tryNumber++;
+          request();
+        }
+      });
+  }
+  request();
 };
 
 export const filterBrand = async ({ filterString, changeItems }: Request) => {
   let tryNumber = 0;
-  const now = new Date().toISOString().split("T")[0];
-  const nowRightFormat =
-    now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
-  await axios({
-    method: "post",
-    url: "http://api.valantis.store:40000/",
-    headers: {
-      "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
-    },
-    data: {
-      action: "filter",
-      params: { brand: filterString },
-    },
-  })
-    .then(function (response) {
-      items = response.data.result;
-      getFields({ changeItems });
+
+  async function request() {
+    const now = new Date().toISOString().split("T")[0];
+    const nowRightFormat =
+      now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
+    await axios({
+      method: "post",
+      url: "http://api.valantis.store:40000/",
+      headers: {
+        "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
+      },
+      data: {
+        action: "filter",
+        params: { brand: filterString },
+      },
     })
-    .catch(function (error) {
-      if (tryNumber === 3) {
-        console.log(error);
-      } else {
-        tryNumber++;
-        filterBrand({ filterString, changeItems });
-      }
-    });
-};
-
-export const getInitialItems = async ({ changeItems }: Request) => {
-  let tryNumber = 0;
-  const now = new Date().toISOString().split("T")[0];
-  const nowRightFormat =
-    now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
-
-  await axios({
-    method: "post",
-    url: "http://api.valantis.store:40000/",
-    headers: {
-      "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
-    },
-    data: {
-      action: "get_ids",
-      params: { offset: 0, limit: 50 },
-    },
-  })
-    .then(function (response) {
-      items = response.data.result;
-      getFields({ changeItems });
-    })
-    .catch(function (error) {
-      if (tryNumber === 3) {
-        console.log(error);
-      } else {
-        tryNumber++;
-        getInitialItems({ changeItems });
-      }
-    });
-};
-
-export const getFields = async ({ changeItems }: Request) => {
-  let tryNumber = 0;
-
-  const now = new Date().toISOString().split("T")[0];
-  const nowRightFormat =
-    now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
-
-  await axios({
-    method: "post",
-    url: "http://api.valantis.store:40000/",
-    headers: {
-      "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
-    },
-    data: {
-      action: "get_items",
-      params: { ids: items },
-    },
-  })
-    .then(function (response) {
-      const result = deleteDublicates(response.data.result);
-      end = result.length;
-      changeItems(result);
-    })
-    .catch(function (error) {
-      if (tryNumber === 3) {
-        console.log(error);
-      } else {
-        tryNumber++;
+      .then(function (response) {
+        items = response.data.result;
         getFields({ changeItems });
-      }
-    });
+      })
+      .catch(function (error) {
+        if (tryNumber === 3) {
+          console.log(error);
+        } else {
+          tryNumber++;
+          request();
+        }
+      });
+  }
+  request();
+};
+
+export const getInitialItems = ({ changeItems }: Request) => {
+  let tryNumber = 0;
+
+  async function request() {
+    const now = new Date().toISOString().split("T")[0];
+    const nowRightFormat =
+      now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
+
+    await axios({
+      method: "post",
+      url: "http://api.valantis.store:40000/",
+      headers: {
+        "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
+      },
+      data: {
+        action: "get_ids",
+        params: { offset: 0, limit: 50 },
+      },
+    })
+      .then(function (response) {
+        items = response.data.result;
+        getFields({ changeItems });
+      })
+      .catch(function (error) {
+        if (tryNumber === 3) {
+          console.log(error);
+        } else {
+          tryNumber++;
+          request();
+        }
+      });
+  }
+  request();
+};
+
+export const getFields = ({ changeItems }: Request) => {
+  let tryNumber = 0;
+  async function request() {
+    const now = new Date().toISOString().split("T")[0];
+    const nowRightFormat =
+      now.split("-")[0] + now.split("-")[1] + now.split("-")[2];
+
+    await axios({
+      method: "post",
+      url: "http://api.valantis.store:40000/",
+      headers: {
+        "X-Auth": Md5.hashStr(`${password}${nowRightFormat}`),
+      },
+      data: {
+        action: "get_items",
+        params: { ids: items },
+      },
+    })
+      .then(function (response) {
+        const result = deleteDublicates(response.data.result);
+        end = result.length;
+        changeItems(result);
+      })
+      .catch(function (error) {
+        if (tryNumber === 3) {
+          console.log(error);
+        } else {
+          tryNumber++;
+          request();
+        }
+      });
+  }
+  request();
 };
